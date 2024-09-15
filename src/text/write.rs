701b40f8398fae482fa87dcd5b53f29494b8b33c
Gabriel Ford <gabe@fordltc.net>
2024-09-14 20:39:58 -0400

    Moved the set pixel color functionality to a method.

--- src/text/write.rs
+++ src/text/write.rs
@@ -18,65 +18,20 @@ impl<'a> Canvas<'a> {
             stride: width * 4,
         }
     }
-    pub fn get_pixel(&mut self, row: u32, column: u32) -> &mut [u8] {
+    pub fn get_pixel(&'a mut self, row: u32, column: u32) -> &'a mut [u8] {
         &mut self[row][column as usize * 4..((column * 4) + 1) as usize]
     }
-}
 
-impl<'a> Index<u32> for Canvas<'a> {
-    type Output = [u8];
-
-    fn index(&self, index: u32) -> &Self::Output {
-        &self.canvas_buffer[(index * self.stride) as usize..((index + 1) * self.stride) as usize]
-    }
-}
-
-impl<'a> IndexMut<u32> for Canvas<'a> {
-    fn index_mut(&mut self, index: u32) -> &mut Self::Output {
-        &mut self.canvas_buffer
-            [(index * self.stride) as usize..((index + 1) * self.stride) as usize]
-    }
-}
-
-pub fn write_text(canvas: Canvas, text: &str) {
-    let Canvas {
-        canvas_buffer,
-        height,
-        width,
-        stride,
-    } = canvas;
-    const TEXT_COLOR: Color = Color::rgb(0xFF, 0xFF, 0xFF);
-    let mut font_system = FontSystem::new();
-    let mut swash_cache = SwashCache::new();
-
-    let font_size: f32 = height as f32 * 0.8;
-    let line_height: f32 = font_size * 1.2;
-
-    let metrics = Metrics::new(font_size, line_height);
-    let mut buffer = Buffer::new(&mut font_system, metrics);
-
-    let mut buffer = buffer.borrow_with(&mut font_system);
-
-    buffer.set_size(Some(width as f32), Some(height as f32));
-
-    let attrs = Attrs::new();
-    let attrs = attrs.family(Family::Name("JetBrainsMono Nerd Font Mono"));
-
-    buffer.set_text(text, attrs, Shaping::Advanced);
-    buffer.shape_until_scroll(true);
-
-    buffer.draw(&mut swash_cache, TEXT_COLOR, |x, y, w, h, color| {
-        let a = color.a();
-        if a == 0 || x < 0 || x >= width as i32 || y < 0 || y >= height as i32 || w != 1 || h != 1 {
-            return;
-        }
+    pub fn set_pixel_color(&mut self, color: Color, x: i32, y: i32) {
+        let stride = self.stride;
 
         let line = stride as i32 * y;
         let chunk_start = (x * 4) + line;
-        if chunk_start as usize > canvas.len() - 4 {
+        if chunk_start as usize > self.canvas_buffer.len() - 4 {
             return;
         }
-        let slice: &mut [u8] = &mut canvas[chunk_start as usize..(chunk_start + 4) as usize];
+        let slice: &mut [u8] =
+            &mut self.canvas_buffer[chunk_start as usize..(chunk_start + 4) as usize];
 
         // Alpha Blending
         let text_a = color.a();
@@ -100,5 +55,59 @@ pub fn write_text(canvas: Canvas, text: &str) {
         slice[2] = text_r;
         slice[1] = text_g;
         slice[0] = text_b;
+    }
+}
+
+impl<'a> Index<u32> for Canvas<'a> {
+    type Output = [u8];
+
+    fn index(&self, index: u32) -> &Self::Output {
+        &self.canvas_buffer[(index * self.stride) as usize..((index + 1) * self.stride) as usize]
+    }
+}
+
+impl<'a> IndexMut<u32> for Canvas<'a> {
+    fn index_mut(&mut self, index: u32) -> &mut Self::Output {
+        &mut self.canvas_buffer
+            [(index * self.stride) as usize..((index + 1) * self.stride) as usize]
+    }
+}
+
+pub fn write_text(mut canvas: Canvas, text: &str) {
+    let Canvas { width, height, .. } = canvas;
+    const TEXT_COLOR: Color = Color::rgb(0xFF, 0xFF, 0xFF);
+    let mut font_system = FontSystem::new();
+    let mut swash_cache = SwashCache::new();
+
+    let font_size: f32 = height as f32 * 0.8;
+    let line_height: f32 = font_size * 1.2;
+
+    let metrics = Metrics::new(font_size, line_height);
+    let mut buffer = Buffer::new(&mut font_system, metrics);
+
+    let mut buffer = buffer.borrow_with(&mut font_system);
+
+    buffer.set_size(Some(width as f32), Some(height as f32));
+
+    let attrs = Attrs::new();
+    let attrs = attrs.family(Family::Name("JetBrainsMono Nerd Font Mono"));
+
+    buffer.set_text(text, attrs, Shaping::Advanced);
+    buffer.shape_until_scroll(true);
+
+    buffer.draw(&mut swash_cache, TEXT_COLOR, |x, y, w, h, color| {
+        println!("w = {w}, width = {width}");
+        let text_a = color.a();
+        if text_a == 0
+            || x < 0
+            || x >= width as i32
+            || y < 0
+            || y >= height as i32
+            || w != 1
+            || h != 1
+        {
+            return;
+        }
+        canvas.set_pixel_color(color, x, y)
     });
 }
